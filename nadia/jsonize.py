c1af0234c6f44321e2bf8183106ac69b910b9b07
Simone Basso <bassosimone@gmail.com>
2011-03-10 16:02:58 +0100

    nadia/jsonize.py: Don't pass empty cells to extras

--- nadia/jsonize.py
+++ nadia/jsonize.py
@@ -93,7 +93,8 @@ def jsonize(data, fp, indent=None):
                 package.tags.append(cell.lower().replace(" ", "-"))
                 continue
 
-            package.extras[header] = cell
+            if cell:
+                package.extras[header] = cell
 
         # XXX
         package.notes = "\n\n".join(package.notes)
